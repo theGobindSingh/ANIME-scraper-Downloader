38a91c9c432a7af7eab4892e86939e7995a4f8c5
GOBIND SINGH <android.gobind@gmail.com>
2021-04-21 07:01:45 +0530

    added percent downloaded

--- Anime-Downloader.py
+++ Anime-Downloader.py
@@ -3,6 +3,8 @@ import sys
 import time
 import json
 import webbrowser
+import warnings
+
 
 
 try:
@@ -39,6 +41,13 @@ except:
     print('m3u8 module not installed! Installing automatically.')
     os.system('python -m pip install m3u8')
     import m3u8
+
+try:
+    from tqdm import tqdm
+except:
+    print('tqdm module not installed! Installing automatically.')
+    os.system('python -m pip install tqdm')
+    from tqdm import tqdm
 
 print("\n     ***************************     ")
 print("     ***************************     ")
@@ -155,7 +164,15 @@ for i in range(sepi,eepi+1):
     url_dl=(str(jsonfd["source"][0]["file"]))
     if not("cloud9xx" in url_dl):
         with open(os.path.join(loc,aniold+"Episode "+str(i)+".mp4"),"wb") as f:
-            f.write(requests.get(url_dl).content) 
+            tempp=requests.get(url_dl,stream=True)
+            #fost="{l_bar}" #: %(percent)3d%%" #[%(bar)s] %(current)d/%(max)d [%(elapsed).1f s] [eta %(eta_avg).0f+-%(eta_stddev).0f s]"
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                for chunk in tqdm(iterable=tempp.iter_content(chunk_size=1024),desc="",total=int(tempp.headers["Content-Length"])/1024,unit="KB",unit_divisor=1024,unit_scale=1,postfix="",bar_format="{l_bar}"):
+                    if chunk:
+                        #print(chunk)
+                        #f.write(tempp.content) 
+                        pass
             f.close()
     else:
         print("\nWarning: This episode was blocked. \n\nNow downloading an alternate file.\nThis download will be very slow;\nBut you can watch it while it is downloading.\n")
@@ -168,7 +185,7 @@ for i in range(sepi,eepi+1):
             urlc=urlc.replace(str(urlc[urlc.rfind('/')+1:]),str(m3u8_master[0]["uri"]))
             temp3=m3u8.loads(requests.get(urlc).text).data["segments"]
             with open(os.path.join(loc,aniold+"Episode "+str(i)+".ts"),"wb") as f:
-                for i in range(0,len(temp3)):
+                for i in tqdm(range(0,len(temp3)),bar_format="{l_bar}"):
                     m3u8_play=temp3[i]["uri"]
                     urlcc=url_dl
                     urlcc=urlcc.replace(str(urlcc[urlcc.rfind('/')+1:]),str(m3u8_play))
