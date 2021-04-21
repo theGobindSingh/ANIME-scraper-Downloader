558c1e7921e5d32f55fe1ef0a96e8777883c6253
GOBIND SINGH <android.gobind@gmail.com>
2021-04-22 01:58:14 +0530

    Updated progress bar settings.
    <3

--- Anime-Downloader.py
+++ Anime-Downloader.py
@@ -163,16 +163,16 @@ for i in range(sepi,eepi+1):
     jsonfd=json.loads(requests.get(urlfj,headers=headers).text)
     url_dl=(str(jsonfd["source"][0]["file"]))
     if not("cloud9xx" in url_dl):
-        with open(os.path.join(loc,aniold+"Episode "+str(i)+".mp4"),"wb") as f:
-            tempp=requests.get(url_dl,stream=True)
-            #fost="{l_bar}" #: %(percent)3d%%" #[%(bar)s] %(current)d/%(max)d [%(elapsed).1f s] [eta %(eta_avg).0f+-%(eta_stddev).0f s]"
-            with warnings.catch_warnings():
-                warnings.filterwarnings("ignore")
-                for chunk in tqdm(iterable=tempp.iter_content(chunk_size=1024),desc="",total=int(tempp.headers["Content-Length"])/1024,unit="KB",unit_divisor=1024,unit_scale=1,postfix="",bar_format="{l_bar}"):
-                    if chunk:
-                        f.write(tempp.content) 
-                        pass
-            f.close()
+        chunk=5120
+        tempp=requests.get(url_dl,stream=True)
+        iterable=tempp.iter_content(chunk_size=chunk)
+        totitr=int(tempp.headers["Content-Length"])/chunk
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore")
+            with open("sample.pdf","wb") as f:
+                for data in tqdm(iterable=iterable,desc="",total=totitr,unit="KB",unit_scale=True,bar_format="{l_bar}{bar}| [{elapsed}]",postfix="",unit_divisor=chunk):
+                    f.write(data)
+                print("\rDownloaded.")
     else:
         print("\nWarning: This episode was blocked. \n\nNow downloading an alternate file.\nThis download will be very slow;\nBut you can watch it while it is downloading.\n")
         yon=input("[+] Do you wish to continue? Enter (y/n)   ").lower()[0]
@@ -183,13 +183,15 @@ for i in range(sepi,eepi+1):
             urlc=url_dl
             urlc=urlc.replace(str(urlc[urlc.rfind('/')+1:]),str(m3u8_master[0]["uri"]))
             temp3=m3u8.loads(requests.get(urlc).text).data["segments"]
-            with open(os.path.join(loc,aniold+"Episode "+str(i)+".ts"),"wb") as f:
-                for i in tqdm(range(0,len(temp3)),bar_format="{l_bar}"):
-                    m3u8_play=temp3[i]["uri"]
-                    urlcc=url_dl
-                    urlcc=urlcc.replace(str(urlcc[urlcc.rfind('/')+1:]),str(m3u8_play))
-                    f.write(requests.get(urlcc).content) 
-                f.close()
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                with open(os.path.join(loc,aniold+"Episode "+str(i)+".ts"),"wb") as f:
+                    for i in tqdm(range(0,len(temp3)),unit="KB",unit_scale=True,bar_format="{l_bar}{bar}| [{elapsed}]",postfix=""):
+                        m3u8_play=temp3[i]["uri"]
+                        urlcc=url_dl
+                        urlcc=urlcc.replace(str(urlcc[urlcc.rfind('/')+1:]),str(m3u8_play))
+                        f.write(requests.get(urlcc).content) 
+                    f.close()
         else:
             print("Ok. Skipping episode "+str(i)+".")
 print("\n***** THANKYOU. HAVE A NICE DAY. *****\n")
